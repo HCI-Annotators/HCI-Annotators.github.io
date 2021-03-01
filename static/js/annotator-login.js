d1a9524a8af6557066fbf73ef8bdbb2e1dfe3f20
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-03-01 12:30:33 -0600

    Add extra login logic

--- static/js/annotator-login.js
+++ static/js/annotator-login.js
@@ -1,6 +1,15 @@
 const login = (ev) => {
   task_id = document.querySelector('#task_id').value;
-  window.location.href = "/annotator-task/#" + task_id;
+  if (task_id) {
+    fetch('/api/tasks/' + task_id + '/')
+        .then(response => response.json())
+        .then(data => {
+          window.location.href = "/annotator-task/#" + task_id;
+        })
+        .catch(error => {
+          document.querySelector('#error').classList.remove("hide");
+        });
+  }
 }
 
 document.querySelector('#start').onclick = login;
