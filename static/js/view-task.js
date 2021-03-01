d3190867edb00c452374cc3fa26fc86f0c4fdc17
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2021-03-01 14:19:36 -0600

    Initial work on task view

---
+++ static/js/view-task.js
@@ -0,0 +1,54 @@
+let activeTask;
+
+const updateTask = (ev) => {
+    const url = window.location.href;
+    id = url.substring(url.lastIndexOf('#') + 1);
+
+    const data = {
+        name: document.querySelector('#name').value,
+        description: document.querySelector('#description').value,
+        instructions: document.querySelector('#instructions').value,
+    };
+    fetch('/api/tasks/' + id + '/', {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(showConfirmation);
+
+    // this line overrides the default form functionality:
+    ev.preventDefault();
+};
+
+const getTask = () => {
+    const url = window.location.href;
+    id = url.substring(url.lastIndexOf('#') + 1);
+
+    fetch('/api/tasks/' + id + '/')
+        .then(response => response.json())
+        .then(data => {
+            activeTask = data;
+            renderTask();
+        });
+}
+
+const renderTask = (ev) => {
+    document.querySelector('#id').innerHTML = "Task ID: " + activeTask._id.$oid;
+    document.querySelector('#name').value = activeTask.name;
+    document.querySelector('#description').value = activeTask.description;
+    document.querySelector('#instructions').value = activeTask.instructions;
+}
+
+const showConfirmation = (data) => {
+    console.log('response from the server:', data);
+    if (data.message && data.id) {
+        document.querySelector('#task-form').classList.toggle("hide");
+        document.querySelector('#confirmation').classList.toggle("hide");
+    }
+};
+
+getTask();
+//document.querySelector('#save').onclick = createTask;
